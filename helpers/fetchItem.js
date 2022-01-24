23a1329a87fd48b1c1ba1c348df323d429391fb1
Kelly Cassiano <keukssie@hotmail.com>
2022-01-24 11:34:17 -0300

    comentando a fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,13 +1,12 @@
-const fetchItem = async (itemID) => {
+const fetchItem = async (itemID) => { // função assíncrona que recebe um item como parâmetro
   // seu código aqui
-const url = `https://api.mercadolibre.com/items/${itemID}`;
- // pega a url e coloca numa variável
+const url = `https://api.mercadolibre.com/items/${itemID}`; // pega a url e coloca numa variável - usa o template literals para acessar o item
  try {
-   const response = await fetch(url);
-   const data = response.json();
-   return data;
- } catch (error) {
-  return `${error}`;
+   const response = await fetch(url); // recebe a api da url e guarda numa variável. Está em await por que ele vai esperar o resultado para poder seguir.
+   const data = response.json(); // pega a api e organiza em json (JavaScript Object Notation), salva numa variáveç
+   return data; // retorna a api
+ } catch (error) { // caso o try não funcione o catch devolve um erro
+  return `${error}`; // retorna uma striing de erro
  }
 };
 
